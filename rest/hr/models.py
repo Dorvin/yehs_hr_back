590b63eba7c18a9a2bfe4d974749798b07be69a9
Dorvin <ladig1017@gmail.com>
2020-08-13 20:30:32 +0900

    fix model, to include max_length attribute

--- rest/hr/models.py
+++ rest/hr/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 class Event(models.Model):
-    name = models.CharField()
-    category = models.CharField()
-    date = models.CharField()       # e.g. 202010 (meaning 2020 year 10 month)
+    name = models.CharField(max_length=1000)
+    category = models.CharField(max_length=1000)
+    date = models.CharField(max_length=1000)       # e.g. 202010 (meaning 2020 year 10 month)
 
 class Member(models.Model):
-    code = models.CharField()       # id of yehs website
-    name = models.CharField()
-    email = models.CharField()
-    phone = models.CharField()
-    yn = models.CharField()         # yehs number
-    fn = models.CharField()         # forum number
-    univ = models.CharField()
-    major = models.CharField()
+    code = models.CharField(max_length=1000)       # id of yehs website
+    name = models.CharField(max_length=1000)
+    email = models.CharField(max_length=1000)
+    phone = models.CharField(max_length=1000)
+    yn = models.CharField(max_length=1000)         # yehs number
+    fn = models.CharField(max_length=1000)         # forum number
+    univ = models.CharField(max_length=1000)
+    major = models.CharField(max_length=1000)
     events = models.ManyToManyField(Event, related_name='participants')
